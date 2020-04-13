c68b68d4b79d6e877bcd41ef2e7a05ec342159aa
SotTeck <63610003+SotTeck@users.noreply.github.com>
2020-04-13 21:14:14 +0200

    Update first.py

--- first.py
+++ first.py
@@ -5,29 +5,8 @@ cpuCores2 = cpuCores
 maxClears = cpuCores/2
 print(maxClears)
 
-def checkIfProcessRunning(processName):
-    '''
-    Check if there is any running process that contains the given name processName.
-    '''
-    #Iterate over the all the running process
-    for proc in psutil.process_iter():
-        try:
-            # Check if process name contains the given name string.
-            if processName.lower() in proc.name().lower():
-                return True
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-    return False;
- 
 def findProcessIdByName(processName):
-    '''
-    Get a list of all the PIDs of a all the running process whose name contains
-    the given string processName
-    '''
- 
     listOfProcessObjects = []
- 
-    #Iterate over the all the running process
     for proc in psutil.process_iter():
        try:
            pinfo = proc.as_dict(attrs=['pid', 'name', 'create_time'])
@@ -43,7 +22,6 @@ def setAffinity(processID):
     proc = psutil.Process(processID)
     proc.cpu_affinity(listik)
 
-# Find PIDs od all the running instances of process by name
 listOfProcessIds = findProcessIdByName('nostaleclientx')
  
 if len(listOfProcessIds) > 0:
@@ -51,7 +29,7 @@ if len(listOfProcessIds) > 0:
    for elem in listOfProcessIds:
        processID = elem['pid']
        processName = elem['name']
-       if cpuCores2 == 2:
+       if cpuCores2 == 0:
            cpuCores2 = cpuCores
        listik = [cpuCores2-1, cpuCores2-2]
        print(processID ,processName, cpuCores2-1, cpuCores2-2)
@@ -59,5 +37,3 @@ if len(listOfProcessIds) > 0:
        setAffinity(processID)
 else :
    print('No Running Process found with given text')
-
-
