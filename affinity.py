598150c94876ac9dd27a54c3cf283cc7532c2570
SotTeck <63610003+SotTeck@users.noreply.github.com>
2020-04-13 21:22:48 +0200

    Update affinity.py

--- affinity.py
+++ affinity.py
@@ -10,7 +10,6 @@ def findProcessIdByName(processName):
     for proc in psutil.process_iter():
        try:
            pinfo = proc.as_dict(attrs=['pid', 'name', 'create_time'])
-           # Check if process name contains the given name string.
            if processName.lower() in pinfo['name'].lower() :
                listOfProcessObjects.append(pinfo)
        except (psutil.NoSuchProcess, psutil.AccessDenied , psutil.ZombieProcess) :
